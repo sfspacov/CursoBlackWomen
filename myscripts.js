1f4988799502d71f24a27e5592fa8b01c15fc64f
Stephan Figueredo Spacov <stephan_fs@hotmail.com>
2021-11-04 18:28:29 -0300

    aula loop for com if

--- myscripts.js
+++ myscripts.js
@@ -1,76 +1,66 @@
-function finalizarCompra()
+function retornarUsuarios()
 {
-	let meusArrays = retornaArray();
-	
-	console.log(meusArrays);
-}
-
-function retornarUsuario()
-{
-	let usuario = 
-	{
-		nome: "Milena",
-		email: "milena@gmail.com",
-		cpf: "178.187.147-81",
-		idade: 20,
-		peso:
-		{
-			valor: 54.70,
-			unidade: "kg"
-		},
-		sangue: 
-		{
-			tipo: "A",
-			rh: "+"
-		}
-	}
-	
-	return usuario;
-}
-
-function retornaArray()
-{
-	let produtos = [ "PS5", "Mouse optico", "Rayban", "Boné" ];
-	let notasP1 = [ 10, 8.5, 0, 2.3, 5 ];
+	//simulando uma busca na base de dados
 	let usuarios = 
 	[
 		{
+			nome: "Angela",
+			email: "angela@gmail.com",
+			saldoDevedor: 0
+		},
+		{
 			nome: "Ana Paula",
-			idade: 30,
-			sangue: 
-			{
-				tipo: "A",
-				rh: "+"
-			}
+			email: "anapaula@gmail.com",
+			saldoDevedor: -100
+		},
+		{
+			nome: "Carla",
+			email: "carla@gmail.com",
+			saldoDevedor: -200
 		},
 		{
 			nome: "Irani",
-			idade: 25,
-			sangue:
-			{
-				tipo: "O",
-				rh: "-"
-			}
-		},
-		{
-			nome: "Verônica",
-			idade: 25,
-			sangue:
-			{
-				tipo: "AB",
-				rh: "+"
-			}
+			email: "irani@gmail.com",
+			saldoDevedor: 0
 		}
-	];	
-	let listaSemTipo = ["string", 1, {}, []];
+	]
 	
-	let retornoDaFuncao =
-	{
-		listaDeProdutos: produtos,
-		listaDeNotas: notasP1,
-		listaDeUsuarios: usuarios,
-		listaMaluca: listaSemTipo
-	}
-	
-	return  retornoDaFuncao
+	return usuarios;
 }
+
+function enviarEmail(usuario)
+{
+	
+	let msg = usuario.nome + "\n" +
+				"Saldo: " + usuario.saldoDevedor + "\n";
+	
+	console.log(msg + 
+	"\nEmail enviado com sucesso para: " 
+	+ usuario.email);
+}
+
+function enviarCobranca()
+{	
+	let usuarios = retornarUsuarios();
+	let tamanho = usuarios.length;	
+	
+	//Loop normal	
+	for (let i = 0; i < tamanho ; i++)
+	{
+		if (usuarios[i].saldoDevedor < 0)
+		{			
+			//envio de email
+			enviarEmail(usuarios[i]);
+		}
+	}
+}
+
+/*
+	console.log("Loop inverso:")
+	for (let i = tamanho - 1; i >= 0 ; i--)
+	{
+		let msg = alunas[i] + "\n";
+
+		console.log(msg)
+	}
+*/
