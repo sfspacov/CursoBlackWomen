52ae7824817898ac5e542bc20d01e8bc2c8ec587
Stephan Figueredo Spacov <stephan_fs@hotmail.com>
2021-11-25 10:00:50 -0300

    Front e Back separados

---
+++ Back/Console/SegundoConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ConsoleApp;
+
+namespace SegundoConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome: ");
+            var nome = Console.ReadLine();
+            Console.WriteLine("Digite seu cpf: ");
+            var cpf = Console.ReadLine();
+
+            var novoUsuario = new Usuario();
+            novoUsuario.Nome = nome;
+            novoUsuario.Cpf = cpf;
+
+            var msg = CriarUsuario(novoUsuario);
+
+            Console.WriteLine(msg);
+        }
+
+        public static string CriarUsuario(Usuario usuario)
+        {
+            return usuario.Salvar(usuario);
+        }
+    }
+}
---
+++ Back/CrudApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CrudApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Oi";
+        }
+    }
+}
